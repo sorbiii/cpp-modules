c1c62c1fec0d4d84cc1f58a8511a190ddd8f22b3
Natalia <nadamczyk3934@gmail.com>
2026-03-24 21:11:38 +0100

    some adjustments

--- module01/ex02/main.cpp
+++ module01/ex02/main.cpp
@@ -5,11 +5,14 @@
 int main(void)
 {
 	std::string s = "HI THIS IS BRAIN";
-	std::string *stringPTR = &s;
-	std::string &stringREF = s;
+	std::string* stringPTR = &s;
+	std::string& stringREF = s;
 
-
-	std::cout << &s << '\n' << &stringPTR << '\n' << &stringREF << '\n';
-	std::cout << s << '\n' << stringPTR << '\n' << stringREF;
+	std::cout << "Memory aderss of string variable: " << &s << std::endl;
+	std::cout << "Memory adress held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory adress held by stringREF: " << &stringREF << std::endl;
+	std::cout << "Value of the string variable: " << s << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl; 
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
 	return (0);
 }
