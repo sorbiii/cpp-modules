1338a9bdbfcc0833059ab27fa0ebc2267cc4831b
Natalia <nadamczyk3934@gmail.com>
2026-03-24 20:42:15 +0100

    ex02 started

--- module02/ex02/Fixed.hpp
+++ module02/ex02/Fixed.hpp
@@ -17,12 +17,20 @@ class Fixed
 		Fixed(const int value);
 		Fixed(const float value);
 		~Fixed();
+
 		int getRawBits(void) const;
 		void setRawBits(int const raw);
 		float toFloat(void) const;
 		int toInt(void) const;
-};
 
+		static Fixed& min(Fixed& a, Fixed& b);
+		static Fixed& min(const Fixed& a, const Fixed& b);
+		static Fixed& max(Fixed& a, Fixed& b);
+		static Fixed& max(const Fixed & a, Fixed& b);
+
+		//operatory
+};
+	
 std::ostream& operator<<(std::ostream& os, const Fixed& obj);
 
 #endif
